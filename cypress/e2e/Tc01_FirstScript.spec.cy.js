5a0b53f8f2591ea11615558de7a4e6b45f3ef121
hacia <haciarpaci12@gmail.com>
2024-08-08 16:41:53 +0300

    fix sign in error

--- cypress/e2e/Tc01_FirstScript.spec.cy.js
+++ cypress/e2e/Tc01_FirstScript.spec.cy.js
@@ -4,7 +4,7 @@ describe('Login Functionality',function(){
 
     it('Login Test using Conduit',function(){
         cy.visit('https://react-redux.realworld.io/')
-        cy.contains('Sign in aa').click()
+        cy.contains('Sign in').click()
         cy.get('input[placeholder="Email"]').type('cypressdemo@gmail.com')
         cy.get('input[placeholder="Password"]').type('cypressdemo')
         cy.get('button[type="submit"]').click()
